0b8a85d3fa28ff3a152bd26624996383d97371e8
Vandor-5676 <greayswordfish@gmail.com>
2025-03-25 18:13:51 -0700

    Add missing "name" attribute to form inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,9 +28,9 @@ document.querySelector(".contact-button").addEventListener("click", () => {
 // Contact Form Validation
 
 function contactForm() {
-	var name = document.getElementById('name');
-	var email = document.getElementById('email');
-	var message = document.getElementById('message');
+	const name = document.getElementById('nameInput');
+	const email = document.getElementById('emailInput');
+	const message = document.getElementById('messageTextArea');
 
 	if(name.value === '' || email.value === '' || message.value === '') {
 		alert("Please Fill Out Each Field.")
