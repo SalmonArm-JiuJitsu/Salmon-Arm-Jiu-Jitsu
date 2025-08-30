9dd4945e52c4b828c5687a66fa04b57507256860
Salmon Arm Jiu-Jitsu <131870121+SalmonArm-JiuJitsu@users.noreply.github.com>
2025-08-30 13:11:20 -0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,6 +53,7 @@ function contactForm() {
 }
 
 
+
 // Register Child Form
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -138,6 +139,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 
+
 // Home Icon Disappear On Scoll
 
 window.addEventListener('scroll', function() {	
@@ -198,15 +200,6 @@ window.addEventListener('scroll', function() {
 
 
 
-
-
-
-
-
-
-
-
-
 // Schedule
 
 const weeklySchedule = [
@@ -215,24 +208,24 @@ const weeklySchedule = [
 	{ day: 'Monday', time: '6:00 PM - 7:15 PM', title: 'Adult No-Gi', capacity: 'available', color: '#303030' },
 
 	// Tuesday	
-	{ day: 'Tuesday', time: '3:15 PM - 3:45 PM', title: 'Tikes (Ages 4-6)', capacity: 'available', color: '#02de91' },
-	{ day: 'Tuesday', time: '4:00 PM - 4:45 PM', title: 'Juniors (Ages 7-9)', capacity: 'available', color: '#f7f757', textColor: '#505050'},
-	{ day: 'Tuesday', time: '5:00 PM - 6:00 PM', title: 'Teens (Ages 10-14) Group A', capacity: 'full', color: '#00d5ff', textColor: '#505050' },
+	{ day: 'Tuesday', time: '3:15 PM - 3:45 PM', title: 'Kids (Ages 4-6)', capacity: 'available', color: '#02de91' },
+	{ day: 'Tuesday', time: '4:00 PM - 4:45 PM', title: 'Kids (Ages 7-9)', capacity: 'available', color: '#f7f757', textColor: '#505050'},
+	{ day: 'Tuesday', time: '5:00 PM - 6:00 PM', title: 'Kids (Ages 10-12)', capacity: 'available', color: '#00d5ff', textColor: '#505050' },
 	{ day: 'Tuesday', time: '6:00 PM - 7:15 PM', title: 'Adult No-Gi', capacity: 'available', color: '#303030' },
 
 	// Wednesday
-	{ day: 'Wednesday', time: '5:00 PM - 6:00 PM', title: 'Teens (Ages 10-14) Group B', capacity: 'available', color: '#de6c02' },
+	{ day: 'Wednesday', time: '5:00 PM - 6:00 PM', title: 'Teens (Ages 13-16)', capacity: 'available', color: '#de6c02' },
 	{ day: 'Wednesday', time: '6:00 PM - 7:15 PM', title: 'Adult No-Gi', capacity: 'available', color: '#303030' },
 
 	// Thursday
-	{ day: 'Thursday', time: '3:15 PM - 3:45 PM', title: 'Tikes (Ages 4-6)', capacity: 'available', color: '#02de91' },
-	{ day: 'Thursday', time: '4:00 PM - 4:45 PM', title: 'Juniors (Ages 7-9)', capacity: 'available', color: '#f7f757', textColor: '#505050'},
-	{ day: 'Thursday', time: '5:00 PM - 6:00 PM', title: 'Teens (Ages 10-14) Group A', capacity: 'full', color: '#00d5ff', textColor: '#505050' },
+	{ day: 'Thursday', time: '3:15 PM - 3:45 PM', title: 'Kids (Ages 4-6)', capacity: 'available', color: '#02de91' },
+	{ day: 'Thursday', time: '4:00 PM - 4:45 PM', title: 'Kids (Ages 7-9)', capacity: 'available', color: '#f7f757', textColor: '#505050'},
+	{ day: 'Thursday', time: '5:00 PM - 6:00 PM', title: 'Kids (Ages 10-12)', capacity: 'available', color: '#00d5ff', textColor: '#505050' },
 	{ day: 'Thursday', time: '6:00 PM - 7:15 PM', title: 'Adult No-Gi', capacity: 'available', color: '#303030' },
 	{ day: 'Thursday', time: '7:15 PM - 8:15 PM', title: 'Ladies No-Gi', capacity: 'available', color: '#fc65dc' },
 
 	// Friday
-	{ day: 'Friday', time: '5:00 PM - 6:00 PM', title: 'Teens (Ages 10-14) Group B', capacity: 'available', color: '#de6c02' },
+	{ day: 'Friday', time: '5:00 PM - 6:00 PM', title: 'Teens (Ages 13-16)', capacity: 'available', color: '#de6c02' },
 	{ day: 'Friday', time: '6:00 PM - 7:15 PM', title: 'Adult No-Gi', capacity: 'available', color: '#303030' },
 
 	// Saturday - Closed
@@ -240,8 +233,9 @@ const weeklySchedule = [
 
 	// Sunday
 	{ day: 'Sunday', time: '10:00 AM - 11:00 AM', title: 'Open Mat', capacity: 'available', color: '#04bd16' },
-	{ day: 'Sunday', time: '12:00 PM - 12:45 PM', title: 'Juniors (Ages 7-9)', capacity: 'available', color: '#f7f757', textColor: '#505050'},
-	{ day: 'Sunday', time: '1:00 PM - 2:00 PM', title: 'Teens (Ages 10-14) Groups A & B', capacity: 'available', color: 'linear-gradient(135deg, #00d5ff 10%, #de6c02 80%)' }
+	{ day: 'Sunday', time: '12:00 PM - 12:45 PM', title: 'Kids (Ages 7-9)', capacity: 'available', color: '#f7f757', textColor: '#505050'},
+	{ day: 'Sunday', time: '1:00 PM - 2:00 PM', title: 'Kids (Ages 10-12)', capacity: 'available', color: '#00d5ff' },
+	{ day: 'Sunday', time: '2:00 PM - 3:00 PM', title: 'Teens (Ages 13-16)', capacity: 'available', color: '#de6c02' }
   
 ];
 
